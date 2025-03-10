35960514948fa6cb46f2d67d6be2be17a1dd6b03
Samuel Izidoro <s.izidoro77@gmail.com>
2025-03-10 14:24:58 -0300

    comit
    Criando um clone do Spotify em HTML, CSS e Js.

---
+++ scripts.js
@@ -0,0 +1,8 @@
+const artistsData = {
+    { name: 'Henrique & Juliano', Image: './img/artista-henrique-jualino.jpeg' },
+    { name: 'Jorge & Mateus', Image: './img/artista-jorge-mateus.jpeg' },
+    { name: 'Zé Neto & Cristiano', Image: './img/artista-henrique-jualino.jpeg' },
+    { name: 'Henrique & Juliano', Image: './img/artista-henrique-jualino.jpeg' },
+    { name: 'Henrique & Juliano', Image: './img/artista-henrique-jualino.jpeg' },
+    { name: 'Henrique & Juliano', Image: './img/artista-henrique-jualino.jpeg' },
+}
