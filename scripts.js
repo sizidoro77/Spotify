ab07d643e40aca145667be4e5298f7aa4a623ad0
Samuel Izidoro <s.izidoro77@gmail.com>
2025-03-11 14:35:06 -0300

    comit
    Primeira parte do Java Script concluida.

--- scripts.js
+++ scripts.js
@@ -1,8 +1,55 @@
-const artistsData = {
-    { name: 'Henrique & Juliano', Image: './img/artista-henrique-jualino.jpeg' },
-    { name: 'Jorge & Mateus', Image: './img/artista-jorge-mateus.jpeg' },
-    { name: 'Zé Neto & Cristiano', Image: './img/artista-henrique-jualino.jpeg' },
-    { name: 'Henrique & Juliano', Image: './img/artista-henrique-jualino.jpeg' },
-    { name: 'Henrique & Juliano', Image: './img/artista-henrique-jualino.jpeg' },
-    { name: 'Henrique & Juliano', Image: './img/artista-henrique-jualino.jpeg' },
-}
+document.addEventListener('DOMContentLoaded', () => {
+    const artistsData = [
+        { name: 'Henrique & Juliano', image: './img/artista-henrique-juliano.jpeg' },
+        { name: 'Jorge & Mateus', image: './img/artista-jorge-mateus.jpeg' },
+        { name: 'Zé Neto e Cristiano', image: './img/artista-ze-neto.jpeg' },
+        { name: 'Gusttavo Lima', image: './img/artista-gustavo-lima.jpeg' },
+        { name: 'Luan Santana', image: './img/artista-luan-santana.jpeg' },
+        { name: 'Matheus & Kauan', image: './img/artista-mateus-kauan.jpeg' }
+    
+      ];
+      
+      const albumsData = [
+        { name: 'White Noise (Sleep & Relaxation Sounds)', artist: 'Sleepy John', image: './img/album-white-noise.jpeg' },
+        { name: 'O Céu Explica Tudo (Ao Vivo)', artist: 'Henrique & Juliano', image: './img/album-ceu-explica.jpeg' },
+        { name: 'Nada como um dia após o outro', artist: 'Racionais', image: './img/album-vida-loka.jpeg' },
+        { name: 'HIT ME HARD AND SOFT', artist: 'Billie Eilish', image: './img/album-hit-me.jpeg' },
+        { name: 'CAJU', artist: 'Liniker', image: './img/album-caju.jpeg' },
+        { name: 'Escândalo Íntimo', artist: 'Luísa Sonza', image: './img/album-escandalo.jpeg' }
+    
+      ];
+
+      const artistGrind = document.querySelector ('.artists-grid')
+      const albumsGrind = document.querySelector ('.albums-grid')
+
+      artistsData.forEach ( artist => {
+        const artistCard = document.createElement('div')
+        artistCard.classList.add('artist-card')
+
+        artistCard.innerHTML = `
+            <img src=${artist.image} "alt=image do ${artist.name}">
+            <h3>${artist.name}</h3>
+            <p>artista</p>
+        
+        `
+
+        artistGrind.appendChild(artistCard)
+    })
+
+      albumsData.forEach ( album=> {
+        const albumCard = document.createElement('div')
+        albumCard.classList.add('album-card')
+
+        albumCard.innerHTML = `
+            <img src=${album.image} "alt=image do ${album.name}">
+            <p>${album.name}</p>
+        
+        `
+
+        albumsGrind.appendChild(albumCard)
+
+      })
+})
+
+
+
