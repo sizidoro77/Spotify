bb36219e7eb5707a6678937be7242236be7d283b
Samuel Izidoro <s.izidoro77@gmail.com>
2025-03-20 13:14:20 -0300

    comit
    Projeto do spotify finalizado.

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
     const artistsData = [
         { name: 'Henrique & Juliano', image: './img/artista-henrique-juliano.jpeg' },
-        { name: 'Jorge & Mateus', image: './img/artista-jorge-mateus.jpeg' },
+        { name: 'Jorge & Mateus', image: './img/artista-jorge-mateus.jpg' },
         { name: 'Zé Neto e Cristiano', image: './img/artista-ze-neto.jpeg' },
-        { name: 'Gusttavo Lima', image: './img/artista-gustavo-lima.jpeg' },
-        { name: 'Luan Santana', image: './img/artista-luan-santana.jpeg' },
+        { name: 'Gusttavo Lima', image: './img/artista-gustavo-limma.png' },
+        { name: 'Luan Santana', image: './img/artista-luan-santana.png' },
         { name: 'Matheus & Kauan', image: './img/artista-mateus-kauan.jpeg' }
     
       ];
@@ -14,7 +14,7 @@ document.addEventListener("DOMContentLoaded", () => {
         { name: 'O Céu Explica Tudo (Ao Vivo)', artist: 'Henrique & Juliano', image: './img/album-ceu-explica.jpeg' },
         { name: 'Nada como um dia após o outro', artist: 'Racionais', image: './img/album-vida-loka.jpeg' },
         { name: 'HIT ME HARD AND SOFT', artist: 'Billie Eilish', image: './img/album-hit-me.jpeg' },
-        { name: 'CAJU', artist: 'Liniker', image: './img/album-caju.jpeg' },
+        { name: 'CAJU', artist: 'Liniker', image: './img/album-caju.jpg' },
         { name: 'Escândalo Íntimo', artist: 'Luísa Sonza', image: './img/album-escandalo.jpeg' }
     
       ];
